5021898559071d3d0001fba203ee776299b98ac5
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2022-04-24 21:20:11 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(j=1;j<=i;j++)         {             printf("%d",i);         }         for(j=i-1;j>=1;j--)         {             printf("%d",i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,9 +5,17 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=n-i;j++)
         {
-            printf("%d ",j);
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        for(j=i-1;j>=1;j--)
+        {
+            printf("%d",i);
         }
         printf("
 ");
